acacebddb6ca519ffdb69dde66d4a5319aad60e1
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2020-03-31 16:53:49 +0530

    Added testing option to gym

--- Main.py
+++ Main.py
@@ -1,18 +1,21 @@
 from Opponents import HumanOpponent, RandomOpponent, HyperionOpponent
 from GameSystem.game import GameEnvironment, Game
-from Agents import HumanAgent,  QLinearAgent
+from Agents import HumanAgent,  QLinearAgent, RandomAgent
 from Gyms import ForwardTDLambdaGym
 
 
 g = GameEnvironment()
-r = RandomOpponent()
+r = RandomOpponent(blocking=True, winning=True)
 h = HyperionOpponent()
 
 agent = QLinearAgent(0.007, 0.2)
+rando = RandomAgent()
 agent_path = "models/QLinearAgent"
 
 
 gym = ForwardTDLambdaGym()
 
-gym.train(agent, g, h, h, episodes = 30000)
-agent.save_model(path=agent_path)
+agent.load_model(path=agent_path)
+#gym.simulate(agent, g, h, h, episodes = 3000, training=False)
+gym.simulate(agent, g, r, r, episodes = 3000, training=False)
+#g.play_slow(agent, h, h)
