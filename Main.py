625ffbce5c2a5c6dd291a6600e8e1c962942b658
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2020-04-28 22:04:29 +0530

    Cleared Main

--- Main.py
+++ Main.py
@@ -5,20 +5,12 @@ from Agents import HumanAgent,  QLinearAgent, RandomAgent, HyperionAgent, NaiveD
 from Gyms import ForwardTDLambdaGym, BatchDQLearningGym
 
 
-
+agent = None
 g = GameEnvironment()
 rdumb = RandomOpponent()
 rsmart = RandomOpponent(blocking=True, winning=True)
 h = HyperionOpponent()
 
-d = DictionaryAgent(0.5, 0.1)
-
-
-
 gym = BatchDQLearningGym(epsilon=0.999, avoid_illegal=True, clear_after_episode=True)
-
-d.load_model(alternate_name="bigdict")
-gym.simulate(d, g, rdumb, rdumb, episodes = 500_000, training=True)
+gym.simulate(agent, g, rdumb, rdumb, episodes = 500_000, training=True)
 #gym.simulate(d, g, r, r, episodes = 3000, training=True)
-d.save_model(alternate_name="dict")
-nz, il, win, inter = d.stats()
